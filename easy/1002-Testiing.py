5b67535e18d73d0ab1039e3faa3a31c0abd59033
Sandijs Buls <c24456862@myTUDublin.ie>
2026-06-29 16:22:31 +0100

    Add solution 1002 - Testiing

---
+++ easy/1002-Testiing.py
@@ -0,0 +1,13 @@
+```python
+"""
+Problem Number: 1002
+Problem Title: Testiing
+Difficulty: Easy
+
+Problem Summary:
+    Hi this is a test claude, write some docstrings to test that you can make comments.
+
+Approach:
+    N/A
+"""
+```
